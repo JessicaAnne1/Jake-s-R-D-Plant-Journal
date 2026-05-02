bf05ab5a6b43f3c07b740e2827f5c4c3e0e8af2a
Jessica O'Donnell <jessica@intrawork.com.au>
2026-05-02 22:08:05 +1000

    Wire frontend to live Apps Script API

--- web/assets/js/config.js
+++ web/assets/js/config.js
@@ -5,7 +5,7 @@
 // safety, set API_TOKEN here AND in apps-script/03_Api.gs.
 
 window.API_CONFIG = {
-  USE_MOCK: true,
-  API_URL: '',     // e.g. 'https://script.google.com/macros/s/AKfyc.../exec'
-  API_TOKEN: '',   // optional shared secret
+  USE_MOCK: false,
+  API_URL: 'https://script.google.com/macros/s/AKfycbxo2iPelXodhnKDxMnPUXpcYq0BG9rtVuhLUUVfyw8qFoAQhqmjDO3qo8uDtU8RHq1jXw/exec',
+  API_TOKEN: '',
 };
